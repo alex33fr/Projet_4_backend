fe9afac4270bac72d42c604401ac302a6e5fed6b
alex33fr <Ser105plm>
2019-05-13 12:26:54 +0200

    Add form Booking.

---
+++ src/Form/BookingType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('buyDate')
+            ->add('visitDate')
+            ->add('durationType')
+            ->add('quantity')
+            ->add('price')
+            ->add('refStripe')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
