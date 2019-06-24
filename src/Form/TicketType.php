993909eb2b61b0ddc6782163da546a6e43f8f1a9
alex33fr <Ser105plm>
2019-06-24 10:47:54 +0200

    Add fill of TicketsType and TicketType

---
+++ src/Form/TicketType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ticket;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TicketType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('birthDate', BirthdayType::class)
+            ->add('specialOffer', CheckboxType::class,[
+                'label' => "Tarif réduit",
+                'required' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ticket::class,
+        ]);
+    }
+}
