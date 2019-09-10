0b62671fc52306cd427d7dfdf81c2ebb059207ac
alex33fr <Ser105plm>
2019-09-10 16:46:27 +0200

    Edit form template text

--- src/Form/TicketType.php
+++ src/Form/TicketType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,13 +16,14 @@ class TicketType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('birthDate', BirthdayType::class)
-            ->add('country', CountryType::class, ['preferred_choices' => ['FR', 'GB']])
+            ->add('firstName', TextType::class, ['label' => 'Prénom'])
+            ->add('lastName', TextType::class, ['label' => 'Nom'])
+            ->add('birthDate', BirthdayType::class, ['label' => 'Né'])
+            ->add('country', CountryType::class, ['label' => 'Pays', 'preferred_choices' => ['FR', 'GB']])
             ->add('specialOffer', CheckboxType::class,[
-                'label' => "Tarif réduit",
-                'required' => false
+                'label' => "Prix réduit",
+                'required' => false,
+                'help' => "Il sera nécessaire de présenter la carte d’étudiant, militaire ou équivalent lors de l’entrée pour prouver le tarif réduit"
             ]);
     }
 
@@ -30,5 +32,9 @@ class TicketType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Ticket::class,
         ]);
+    }
+
+    private function addFlash(string $string, string $string1)
+    {
     }
 }
