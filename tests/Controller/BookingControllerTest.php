84a06eb8a0effbab1652e985a698e355af80734d
alex33fr <Ser105plm>
2019-09-10 16:40:21 +0200

    Create Booking Controller Test

---
+++ tests/Controller/BookingControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BookingControllerTest extends WebTestCase
+{
+
+    public function testOrderTicket()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $form = $crawler->selectButton('CONTINUER')->form();
+        $form['booking[visitDate]'] = '2019-10-21';
+        $form['booking[durationType]'] = 1;
+        $form['booking[quantity]'] = 1;
+        $form['booking[email]'] = 'a@a.a';
+        $client->submit($form);
+
+        $crawler = $client->followRedirect();
+
+
+        $this->assertSame('Formulaire',$crawler->filter('h2')->text());
+
+    }
+}
