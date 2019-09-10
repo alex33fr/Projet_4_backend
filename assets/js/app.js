fbe01e8fa3e403564d0550a4c73da856f7598521
alex33fr <Ser105plm>
2019-09-10 16:34:21 +0200

    Add styles on design

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,9 @@
 
 // any CSS you require will output into a single css file (app.scss in this case)
 require('../css/app.scss');
+require('bootstrap');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+//console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
