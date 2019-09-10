1fea50958f2e0b5ab0150d68aa6ed8c9d6de1cbe
alex33fr <Ser105plm>
2019-09-10 17:05:44 +0200

    Remouve useless holydays

--- src/Validator/NotHolydaysValidator.php
+++ src/Validator/NotHolydaysValidator.php
@@ -1,18 +1,11 @@
 <?php
 
 namespace App\Validator;
-
-
-use App\Repository\BookingRepository;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class NotHolydaysValidator extends ConstraintValidator
 {
-
-    public function __construct(BookingRepository $bookingRepository)
-    {
-    }
 
     public function validate($value, Constraint $constraint)
     {
@@ -22,24 +15,11 @@ class NotHolydaysValidator extends ConstraintValidator
             return;
         }
 
-        $easterDate = easter_date($value->format('Y'));
-        $easterDay = date('j', $easterDate);
-        $easterMonth = date('n', $easterDate);
-        $easterYear = date('Y', $easterDate);
-
         $holidays = array(
-            // Jours feries fixes
-            mktime(0, 0, 0, 1, 1, $value->format('Y')),// 1er janvier
+            // Jours fériés fixes
             mktime(0, 0, 0, 5, 1, $value->format('Y')),// Fete du travail
-            mktime(0, 0, 0, 5, 8, $value->format('Y')),// Victoire des allies
-            mktime(0, 0, 0, 7, 14, $value->format('Y')),// Fete nationale
-            mktime(0, 0, 0, 8, 15, $value->format('Y')),// Assomption
             mktime(0, 0, 0, 11, 1, $value->format('Y')),// Toussaint
-            mktime(0, 0, 0, 11, 11, $value->format('Y')),// Armistice
-            mktime(0, 0, 0, 12, 25, $value->format('Y')),// Noel
-            mktime(0, 0, 0, $easterMonth, $easterDay + 1, $easterYear),// Lundi de paques
-            mktime(0, 0, 0, $easterMonth, $easterDay + 39, $easterYear),// Ascension
-            mktime(0, 0, 0, $easterMonth, $easterDay + 50, $easterYear), // Pentecote
+            mktime(0, 0, 0, 12, 25, $value->format('Y')),// Noël
         );
         if (in_array($value->format('U'), $holidays)) {
             $this->context->buildViolation($constraint->message)
